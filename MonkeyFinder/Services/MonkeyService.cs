24b4768074c5936b0386c9f7cd241d3279f359ee
angel <angel@LAPTOP-FQ5253PJ>
2022-10-10 19:39:47 -0500

    Agregar archivos de proyecto.

---
+++ MonkeyFinder/Services/MonkeyService.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
+
+public class MonkeyService
+{
+    HttpClient httpClient;
+    public MonkeyService()
+    {
+        this.httpClient = new HttpClient();
+    }
+
+    List<Monkey> monkeyList;
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        if (monkeyList?.Count > 0)
+            return monkeyList;
+
+        // JSON ONLINE
+        var response = await httpClient.GetAsync("https://raw.githubusercontent.com/ztmlegna/PerrosApi/main/db.json");
+        if (response.IsSuccessStatusCode)
+        {
+            monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+        }
+
+        // Offline
+        /*using var stream = await FileSystem.OpenAppPackageFileAsync("monkeydata.json");
+        using var reader = new StreamReader(stream);
+        var contents = await reader.ReadToEndAsync();
+        monkeyList = JsonSerializer.Deserialize<List<Monkey>>(contents);*/
+
+        return monkeyList;
+    }
+}
---
+++ MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -0,0 +1,58 @@
+﻿using MonkeyFinder.Services;
+
+namespace MonkeyFinder.ViewModel;
+
+public partial class MonkeysViewModel : BaseViewModel
+{
+    public ObservableCollection<Monkey> Monkeys { get; } = new();
+
+    MonkeyService monkeyService;
+
+    public MonkeysViewModel(MonkeyService monkeyService)
+    {
+        Title = "Veterinaria perrito feliz";
+        this.monkeyService = monkeyService;
+    }
+
+    [RelayCommand]
+    async Task GoToDetailsAsync(Monkey monkey)
+    {
+        if (monkey is null) 
+        return;
+        await Shell.Current.GoToAsync($"{nameof(DetailsPage)}", true,
+            new Dictionary<string, object> 
+            {
+                {"Monkey", monkey}
+        });
+    }
+
+    [RelayCommand]
+    async Task GetMonkeysAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+            var monkeys = await monkeyService.GetMonkeys();
+
+            if(Monkeys.Count != 0)
+                Monkeys.Clear();
+                
+            foreach(var monkey in monkeys)
+                Monkeys.Add(monkey);
+
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"No se puede abrir: {ex.Message}");
+            await Shell.Current.DisplayAlert("Error", ex.Message, "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+
+    }
+}
